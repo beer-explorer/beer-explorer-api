dc43443d3c8aa0d3c91d25f8441ee88c1aefb768
rdrgzma <rdrgzma@hotmail.com>
2018-02-13 02:13:36 -0200

    add token authorization controlleres and middlewares

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 require('./controllers/authController')(app);
+require('./controllers/placeController')(app);
 
 app.get('/',(req,res)=>{
     res.send('ok');
